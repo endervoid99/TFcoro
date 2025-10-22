503754b92fd103eee0582dae7b2f3842111b780e
Thomas Fischer <endervoid99@gmail.com>
2025-10-22 08:26:50 +0200

    wip

---
+++ src/tfcoro_core.cpp
@@ -0,0 +1,12 @@
+// TFCoro Core Implementation
+#include "tfcoro.h"
+
+// This is a placeholder implementation file
+// Add your actual TFCoro implementation here
+
+namespace tfcoro {
+    // Example function implementation
+    void initialize() {
+        // Initialization code here
+    }
+}
